d5219c435dad8678a37a816d00c8baa349c11c14
Noah <noah@gojo.local>
2024-10-26 01:21:39 +0200

    fix: clean up settings.sh

--- settings.sh
+++ settings.sh
@@ -6,12 +6,13 @@ defaults write com.apple.dock largesize -int 90
 defaults write com.apple.dock magnification -bool true
 defaults write com.apple.dock show-recents -bool false
 
-# Reduce transparency to make menu bar dark
-defaults write com.apple.universalaccess reduceTransparency -bool true
-
 # Enable option to show battery number
 defaults write com.apple.menuextra.battery ShowPercent -bool true
 
 # Enable single-tap on trackpad
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+
+# Make dock reveal faster
+defaults write com.apple.dock autohide-delay -bool false
