2bb38f3776772db24c9df823c07009d27f385bb3
Noah <noah@gojo.local>
2024-10-27 18:02:26 +0100

    fix: install ansible

--- python.sh
+++ python.sh
@@ -5,8 +5,8 @@
 source ~/dev/venv/ansible/bin/activate
 which python3
 python3 -m pip install --upgrade pip
-python3 -m pip install ansible==2.9
-dectivate
+python3 -m pip install ansible
+deactivate
 
 # Basic environment
 [ ! -d ~/dev/venv/basicenv ] && python3 -m venv ~/dev/venv/basicenv
@@ -14,4 +14,4 @@ source ~/dev/venv/basicenv/bin/activate
 which python3
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-dectivate
+deactivate
