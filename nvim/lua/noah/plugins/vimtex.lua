116d2aeb0a89691bb59995b59b7494cdc3d919c7
trioxtron <trioxgames@gmail.com>
2026-01-21 09:22:45 +0100

    feat: vimtex latexmk and quickfix

--- nvim/lua/noah/plugins/vimtex.lua
+++ nvim/lua/noah/plugins/vimtex.lua
@@ -5,8 +5,9 @@ return {
     -- VimTeX configuration goes here, e.g.
     --vim.g.tex_flavor = "latex"
     vim.g.vimtex_view_method = "zathura"
-    --vim.g.vimtex_compiler_method = "latexmk"
+    vim.g.vimtex_compiler_method = "latexmk"
     vim.g.vimtex_quickfix_mode = 1
+    vim.g.vimtex_quickfix_open_on_warning = 0
     --vim.g.tex_conceal = "abdmg"
   end
 }
